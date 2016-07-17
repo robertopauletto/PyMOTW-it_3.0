e51139f65715046be2390a2861471e03497ca873
Roberto Pauletto <roberto.pauletto@gmail.com>
2016-07-17 18:29:34 +0200

    Spell checker - first draft

--- site_builder/spell_checker.py
+++ site_builder/spell_checker.py
@@ -4,13 +4,16 @@
 __date__=''
 __version__='0.1'
 __doc__="""
-Spell checker
+Integrazione di uno spell checker per il file .xml di traduzione
 """
 
 try:
     from enchant.checker import SpellChecker
-except:
-    raise ImportError("E' richiesto il modulo enchant")
+    from bs4 import BeautifulSoup
+except ImportError as ierr:
+    no_module = ierr.message.split()[-1]
+    raise ImportError("E' richiesto il modulo " + no_module)
+
 
 class EnchantProxy(object):
     
@@ -18,5 +21,29 @@ class EnchantProxy(object):
         self._lang = lang
         self._chrk = SpellChecker(lang)
         
-    
-        
+class SpellCheck(object):
+    """Contiene la logica per la verifica della sintassi del file xml di
+    traduzione - viene verificato il testo contenuto nei tag definiti nella
+    variabile di classe tags_to_check"""
+    tags_to_check = ['testo_normale', 'note']
+
+    def __init__(self, to_check):
+        """Ottiene il testo da verificare"""
+        self._soup = BeautifulSoup(to_check, "lxml")
+        self._ep = EnchantProxy()
+        self._chunks = self._get_chunks()
+
+    def _get_chunks(self):
+        """Estrae gli elementi da verificare specificati in tags_to_check"""
+        retval = []
+        for tag in SpellCheck.tags_to_check:
+            retval.extend(self._soup.findAll(tag))
+        return retval
+
+    def get_checked(self, is_prettyprinted=False):
+        """([bool]) -> str
+
+        Ritorna il testo corretto"""
+        return str(self._soup) if not is_prettyprinted \
+            else self._soup.prettify()
+
