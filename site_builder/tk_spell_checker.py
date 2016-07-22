e85e0850dac3b31b3986b7962f6a5d422a19ba22
Roberto Pauletto <roberto.pauletto@gmail.com>
2016-07-22 21:27:08 +0200

    Completata stesura interfaccia grafica

--- site_builder/tk_spell_checker.py
+++ site_builder/tk_spell_checker.py
@@ -18,6 +18,9 @@ class Gui(object):
 
     def __init__(self, root, geometry="800x600+100+200"):
 
+        self._new_word = StringVar()
+        self._txt = None
+        self._lbhints = None
         self.root = root
         self.root.title("Spell Checker")
         self.root.wm_iconbitmap('@spell_checking.xbm')
@@ -25,6 +28,7 @@ class Gui(object):
             self.root.geometry(geometry)
         self._file = StringVar(value='Scegliere il file da correggere')
         self._draw()
+        self._fill_hints(None)
 
     def _draw(self):
         fm = ttk.LabelFrame(self.root, text=" File da tradurre ")
@@ -35,33 +39,83 @@ class Gui(object):
             fm, text='Sfoglia'
         ).grid(row=0, column=1, padx=5, pady=5, sticky=EW)
         fm.columnconfigure(0, weight=2)
-
         fm.grid(row=0, column=0, padx=8, pady=8, sticky=EW)
 
-        fmNav = ttk.LabelFrame(self.root, text=' Navigazione ')
+        # ------------------------------------------------------
+
+        fm_hud = ttk.LabelFrame(self.root, text=" Comandi e Navigazione ")
+
+        fm_nv = ttk.LabelFrame(fm_hud, text=' Navigazione ')
         Button(
-            fmNav, text='<<'
+            fm_nv, text='<<'
+        ).grid(row=0, column=0, padx=5, pady=5)
+        Button(
+            fm_nv, text='<'
+        ).grid(row=0, column=1, padx=5, pady=5)
+        Button(
+            fm_nv, text='>'
         ).grid(row=0, column=2, padx=5, pady=5)
         Button(
-            fmNav, text='<'
+            fm_nv, text='>>'
         ).grid(row=0, column=3, padx=5, pady=5)
+        fm_nv.grid(row=0, column=0, ipady=17)
+
+        # ------------------------------------------------------
+
+        fm_cmd = ttk.LabelFrame(fm_hud, text=' Comandi ')
+        Entry(
+            fm_cmd, width=20, textvariable=self._new_word
+        ).grid(row=0, column=0, padx=5, pady=5)
         Button(
-            fmNav, text='>'
-        ).grid(row=0, column=4, padx=5, pady=5)
+            fm_cmd, text='Correggi', command=self._correggi, width=10
+        ).grid(row=0, column=1, padx=5, pady=5)
         Button(
-            fmNav, text='>>'
-        ).grid(row=0, column=5, padx=5, pady=5)
-        fmNav.columnconfigure(0, weight=0)
-        fmNav.columnconfigure(1, weight=0)
-        fmNav.columnconfigure(2, weight=1)
-        fmNav.columnconfigure(3, weight=1)
-        fmNav.columnconfigure(4, weight=1)
-        fmNav.columnconfigure(5, weight=1)
-        fmNav.grid(row=1, column=0, padx=8, pady=8, sticky=EW)
+            fm_cmd, text='Ignora', command=self._correggi, width=10
+        ).grid(row=0, column=2, padx=5, pady=5)
+        Button(
+            fm_cmd, text='Ignora Sempre', command=self._correggi, width=10
+        ).grid(row=1, column=1, padx=5, pady=5, sticky=E)
+        Button(
+            fm_cmd, text='Aggiungi a diz.', command=self._correggi, width=10
+        ).grid(row=1, column=2, padx=5, pady=5, sticky=E)
+        fm_cmd.grid(row=0, column=1, padx=8, pady=8, sticky=EW)
+
+        fm_hud.grid(row=1, column=0, padx=8, pady=8, sticky=EW)
+        fm_hud.columnconfigure(0, weight=1)
+        fm_hud.columnconfigure(1, weight=8)
+        # ------------------------------------------------------
+
+        fm_data = ttk.LabelFrame(self.root, text=" Contesto e Suggerimenti ")
+
+        fm_txt = ttk.LabelFrame(fm_data, text=' Contesto ')
+        self._txt = Text(fm_txt, width=77, height=12)
+        self._txt.grid(row=0, column=1, padx=5, pady=5, sticky=EW)
+        fm_txt.grid(row=0, column=0, padx=8, pady=8, sticky=EW)
+
+        fm_hints = ttk.LabelFrame(fm_data, text=' Suggerimenti ')
+        self._lbhints = Listbox(fm_hints, selectmode=SINGLE)
+        self._lbhints.grid(row=0, column=1, padx=8, pady=8, sticky=EW)
+        fm_hints.grid(row=0, column=1, padx=8, pady=8, sticky=EW, ipady=8)
+
+        fm_data.grid(row=2, column=0, padx=8, pady=8, sticky=EW)
+        fm_data.columnconfigure(0, weight=8)
+        fm_data.columnconfigure(1, weight=1)
+        # ------------------------------------------------------
+        fm_save = ttk.LabelFrame(self.root, text=" Conferme ")
+        Button(
+            fm_save, text='Salva Modifiche', command=self._correggi, width=10
+        ).grid(row=0, column=0, padx=5, pady=5, sticky=EW)
+        fm_save.grid(row=3, column=0, padx=8, pady=8, sticky=EW)
 
 
         self.root.columnconfigure(0, weight=2)
 
+    def _correggi(self):
+        pass
+
+    def _fill_hints(self, hints):
+        for i in range(10):
+            self._lbhints.insert(END, "hint n. " + str(i))
 
 if __name__ == '__main__':
     root = Tk()
